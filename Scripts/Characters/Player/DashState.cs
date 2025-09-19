4881771e68673652bac0a97fcbce779772c94d63
dreischild <ds@dreischild.com>
2025-09-19 12:12:36 +0200

    Dash Animation und Script hinzufügen

---
+++ Scripts/Characters/Player/DashState.cs
@@ -0,0 +1,44 @@
+namespace GameDevTutorial25DRPGGameDevTvGodot.Scripts.Characters.Player;
+
+using GameDevTutorial25DRPGGameDevTvGodot.Scripts.General;
+using Godot;
+
+public partial class DashState : Node
+{
+    private Player player;
+
+    public override void _Ready()
+    {
+        // Über die GetOwner-Methode wird der Player-Node (Root-Node) abgerufen,
+        // da dieser das Eltern-Node ist. Über <Player> wird der Typ gecastet.
+        player = GetOwner<Player>();
+
+        // Sicherstellen, dass die PhysicsProcess-Methode standardmäßig nicht aufgerufen wird.
+        Notification(StateMachine.STOP_PHYSICS_PROCESS_NOTIFICATION);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        
+    }
+    
+    // Die Notification-Methode wird aufgerufen, wenn eine Notification an diesen Node geschickt wird.
+    public override void _Notification(int what)
+    {
+        // Es muss sichergestellt werden, dass die base Notification-Methode aufgerufen wird.
+        base._Notification(what);
+
+        if (what == StateMachine.START_ANIMATION_NOTIFICATION)
+        {
+            player.animationPlayer.Play(GameConstants.ANIM_DASH);
+        }
+        else if (what == StateMachine.START_PHYSICS_PROCESS_NOTIFICATION)
+        {
+            SetPhysicsProcess(true);
+        }
+        else if (what == StateMachine.STOP_PHYSICS_PROCESS_NOTIFICATION)
+        {
+            SetPhysicsProcess(false);
+        }
+    }
+}
