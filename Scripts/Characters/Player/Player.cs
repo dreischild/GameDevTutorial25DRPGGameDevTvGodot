d376583736318ccdfc4c6c10bc3e3e67e7805cf4
dreischild <ds@dreischild.com>
2025-09-23 14:14:14 +0200

    Auto Properties

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -8,8 +8,8 @@ public partial class Player : CharacterBody3D
 {
     [ExportGroup("Required Nodes")]
     [Export] private Sprite3D sprite3dNode;
-    [Export] public AnimationPlayer animationPlayer;
-    [Export] public StateMachine stateMachine;
+    [Export] public AnimationPlayer animationPlayer { get; private set; }
+    [Export] public StateMachine stateMachine { get; private set; }
 
     public Vector2 direction = Vector2.Zero;
     private StringName currentAnimation = GameConstants.ANIM_IDLE;
