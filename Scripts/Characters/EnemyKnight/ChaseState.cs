5f6a072e20cf4dc82ce5acd60f90ec1383d762f3
dreischild <ds@dreischild.com>
2025-09-26 11:58:23 +0200

    Timer zur Neuberechnung

--- Scripts/Characters/EnemyKnight/ChaseState.cs
+++ Scripts/Characters/EnemyKnight/ChaseState.cs
@@ -6,13 +6,30 @@ using System;
 
 public partial class ChaseState : EnemyKnightState
 {
+    [Export] public Timer recalculationTimer { get; private set; }
+
     protected override void EnterState()
     {
         // Sicherstellen, dass die Move Animation abgespielt wird.
-        character.animationPlayer.Play(GameConstants.ANIM_MOVE);        
+        character.animationPlayer.Play(GameConstants.ANIM_MOVE);
+
+        recalculationTimer.Timeout += RecalculateTargetPosition;
+
+        RecalculateTargetPosition();
+    }
+
+    protected override void LeaveState()
+    {
+        recalculationTimer.Timeout -= RecalculateTargetPosition;
     }
 
     public override void _PhysicsProcess(double delta)
+    {
+        // Zur Ziel Position bewegen.
+        GoToNavigationTarget(6f);
+    }
+
+    private void RecalculateTargetPosition()
     {
         // Abbruch, wenn der Spieler nicht mehr verfügbar oderim Sichtbereich ist.
         if (character.targetCharacter == null || !character.CanSeeObject(character.targetCharacter))
@@ -24,8 +41,5 @@ public partial class ChaseState : EnemyKnightState
 
         // Ziel Position auf Position des Spielers setzen.
         character.navigationAgent3DNode.TargetPosition = character.targetCharacter.GlobalPosition;
-
-        // Zur Ziel Position bewegen.
-        GoToNavigationTarget(6f);
     }
 }
